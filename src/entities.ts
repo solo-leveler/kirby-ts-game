7ceb98b7f9d65e2d7efb03d2f8fb3a72c8172256
solo-leveler <shinichiizumi@protonmail.ch>
2024-06-26 21:27:51 +0530

    adding entities

---
+++ src/entities.ts
@@ -0,0 +1,82 @@
+import { GameObj, KaboomCtx } from "kaboom";
+import { scale } from "./constants";
+
+export function makePlayer(k: KaboomCtx, posX: number, posY: number) {
+  const player = k.make([
+    k.sprite("assets", { anim: "kirbIdle" }),
+    k.area({ shape: new k.Rect(k.vec2(4, 5.9), 8, 10) }),
+    k.body(),
+    k.pos(posX * scale, posY * scale),
+    k.scale(scale),
+    k.doubleJump(10),
+    k.health(3),
+    k.opacity(1),
+    {
+      speed: 300,
+      direction: "right",
+      isInhaling: false,
+      isFull: false,
+    },
+    "player",
+  ]);
+
+  player.onCollide("enemy", async (enemy: GameObj) => {
+    if (player.isInhaling && enemy.isInhalable) {
+      player.isInhaling = false;
+      k.destroy(enemy);
+      player.isFull = true;
+      return;
+    }
+    if (player.hp() == 0) {
+      k.destroy(player);
+      k.go("level-2");
+      return;
+    }
+    player.hurt();
+
+    await k.tween(
+      player.opacity,
+      0,
+      0.05,
+      (val) => (player.opacity = val),
+      k.easings.linear
+    );
+
+    await k.tween(
+      player.opacity,
+      1,
+      0.05,
+      (val) => (player.opacity = val),
+      k.easings.linear
+    );
+  });
+
+  player.onCollide("exit", () => {
+    k.go("level-2");
+  });
+
+  const inhaleEffect = k.add([
+    k.sprite("assets", { anim: "kirbInhalingEffect" }),
+    k.pos(),
+    k.scale(scale),
+    k.opacity(0),
+    "inhaleEffect",
+  ]);
+
+  const inhaleZone = player.add([
+    k.area({ shape: new k.Rect(k.vec2(0), 20, 4) }),
+    k.pos(),
+    "inhaleZone",
+  ]);
+
+  inhaleZone.onUpdate(() => {
+    if (player.direction == "left") {
+      inhaleZone.pos = k.vec2(-14, 8);
+      inhaleEffect.pos = k.vec2(player.pos.x - 60, player.pos.y + 0);
+      inhaleEffect.flipX = true;
+    }
+    inhaleZone.pos = k.vec2(14, 8);
+    inhaleEffect.pos = k.vec2(player.pos.x + 60, player.pos.y + 0);
+    inhaleEffect.flipX = false;
+  });
+}
